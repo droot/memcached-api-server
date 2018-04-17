adce5178c9102b166bb265fdac7926f09fc091c4
Sunil Arora <sunilarora@google.com>
2018-04-17 11:29:29 -0700

    initial import

---
+++ pkg/inject/zz_generated.kubebuilder.go
@@ -0,0 +1,63 @@
+package inject
+
+import (
+	myappsv1alpha1 "github.com/droot/memcached-api-server/pkg/apis/myapps/v1alpha1"
+	rscheme "github.com/droot/memcached-api-server/pkg/client/clientset/versioned/scheme"
+	"github.com/droot/memcached-api-server/pkg/controller/memcached"
+	"github.com/droot/memcached-api-server/pkg/inject/args"
+	"github.com/kubernetes-sigs/kubebuilder/pkg/inject/run"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/scheme"
+)
+
+func init() {
+	rscheme.AddToScheme(scheme.Scheme)
+
+	// Inject Informers
+	Inject = append(Inject, func(arguments args.InjectArgs) error {
+		Injector.ControllerManager = arguments.ControllerManager
+
+		if err := arguments.ControllerManager.AddInformerProvider(&myappsv1alpha1.Memcached{}, arguments.Informers.Myapps().V1alpha1().Memcacheds()); err != nil {
+			return err
+		}
+
+		// Add Kubernetes informers
+		if err := arguments.ControllerManager.AddInformerProvider(&appsv1.ReplicaSet{}, arguments.KubernetesInformers.Apps().V1().ReplicaSets()); err != nil {
+			return err
+		}
+		if err := arguments.ControllerManager.AddInformerProvider(&corev1.Pod{}, arguments.KubernetesInformers.Core().V1().Pods()); err != nil {
+			return err
+		}
+		if err := arguments.ControllerManager.AddInformerProvider(&appsv1.Deployment{}, arguments.KubernetesInformers.Apps().V1().Deployments()); err != nil {
+			return err
+		}
+
+		if c, err := memcached.ProvideController(arguments); err != nil {
+			return err
+		} else {
+			arguments.ControllerManager.AddController(c)
+		}
+		return nil
+	})
+
+	// Inject CRDs
+	Injector.CRDs = append(Injector.CRDs, &myappsv1alpha1.MemcachedCRD)
+	// Inject PolicyRules
+	Injector.PolicyRules = append(Injector.PolicyRules, rbacv1.PolicyRule{
+		APIGroups: []string{"myapps.memcached.example.com"},
+		Resources: []string{"*"},
+		Verbs:     []string{"*"},
+	})
+	// Inject GroupVersions
+	Injector.GroupVersions = append(Injector.GroupVersions, schema.GroupVersion{
+		Group:   "myapps.memcached.example.com",
+		Version: "v1alpha1",
+	})
+	Injector.RunFns = append(Injector.RunFns, func(arguments run.RunArguments) error {
+		Injector.ControllerManager.RunInformersAndControllers(arguments)
+		return nil
+	})
+}
